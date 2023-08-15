5479b4df637a934172686b967dce0e21f9e8a4ff
Dirk Allmendinger <dirk.allmendinger@web.de>
2023-08-15 21:16:44 +0200

    add debug mail

--- python/streamLink.py
+++ python/streamLink.py
@@ -195,6 +195,12 @@ search = True
 #first date afer header range
 rowNumber=6
 
+#testsection send a mail when activated
+senderTestList = ( "dirk.allmendinger@web.de")
+emailSubjectString = "streamlink erfolgreich gestartet"
+emailContentString = "Hallo,\nstreamlink wurde erfolgreich gestartet und ist vor dem Datumsvergleich"
+send_message(service, senderTestList, emailSubjectString, emailContentString, [ ])
+
 #now walk over row, pick the first column and compare the date with the selected date
 while search:
     rowNumber +=1
