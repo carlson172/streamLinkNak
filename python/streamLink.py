79e0c923f7d3a2e86dd61f86354080a92498da5f
Dirk Allmendinger <dirk.allmendinger@web.de>
2023-08-19 21:26:49 +0200

    cha

--- python/streamLink.py
+++ python/streamLink.py
@@ -28,6 +28,7 @@ from datetime import date, datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 # for encoding/decoding messages in base64
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 # for dealing with attachement MIME types
@@ -69,18 +70,23 @@ def gmail_authenticate():
     creds = None
     # the file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first time
-    if os.path.exists(goMailCred):
-        with open(goMailCred, "rb") as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
     # if there are no (valid) credentials availablle, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                print("Credentials could not be refreshed, possibly the authorization was revoked by the user.")
+                os.unlink(goMailCred)
+                return
         else:
             flow = InstalledAppFlow.from_client_secrets_file(goMailCred, SCOPES)
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
-        with open(goMailCred, "wb") as token:
+        with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
     return build('gmail', 'v1', credentials=creds)
 
